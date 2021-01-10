a092d41e6e6f82edb0712c462548b9a78648b969
Esteban R <70593544+XzzIsor@users.noreply.github.com>
2021-01-10 16:33:23 -0500

    Update2

---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_first_await/src/screens/alert_page.dart';
+import 'package:flutter_first_await/src/screens/avatar_page.dart';
+import 'package:flutter_first_await/src/screens/home_page.dart';
+
+Map<String, WidgetBuilder> getPageRoutes() {
+  return <String, WidgetBuilder>{
+    '/': (BuildContext context) => HomePage(),
+    'alert': (BuildContext context) => AlertPage(),
+    'avatar': (BuildContext context) => AvatarPage(),
+  };
+}
