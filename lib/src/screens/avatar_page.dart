fe6680a904ff0a46a823b810a6b206c99dea140b
Esteban R <70593544+XzzIsor@users.noreply.github.com>
2021-01-18 16:56:41 -0500

    Update4

--- lib/src/screens/avatar_page.dart
+++ lib/src/screens/avatar_page.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
 class AvatarPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Avatar"),
-      ),
-      body: Center(
-        child: Text("hola"),
-      )
-
-    );
+        appBar: AppBar(
+          title: Text("Avatar"),
+          
+          actions: <Widget>[
+             CircleAvatar(
+              child: Text('JY'),
+              backgroundColor: Colors.deepOrangeAccent,
+            ),
+            SizedBox(width: 10),
+            CircleAvatar(
+              radius: 20,
+              backgroundImage: NetworkImage('https://e00-elmundo.uecdn.es/elmundo/imagenes/2007/08/06/1186391983_0.jpg'),
+              ),
+            SizedBox(width: 10,),
+          ],
+        ),
+        body: Center(
+            child: FadeInImage(
+          image: NetworkImage(
+              "https://e00-elmundo.uecdn.es/elmundo/imagenes/2007/08/06/1186391983_0.jpg"),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeOutDuration: Duration(milliseconds: 300),
+        )));
   }
 }
